b979bba2aeffff9dadd026d87e8af1f2615ba43d
Onyekachi Stanley <onyekachila@gmail.com>
2019-04-22 16:01:28 +0100

    Add components with vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -24,8 +24,8 @@ Vue.use(Vuetify)
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
+Vue.component('AppHome', require('./components/AppHome.vue').default);
+import router from './Router/router.js';
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -33,5 +33,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <toolbar></toolbar>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Toolbar from "./Toolbar";
+import AppFooter from "./AppFooter";
+
+export default {
+  components: {
+    Toolbar,
+    AppFooter
+  }
+};
+</script>
+
+<style>
+</style>
